07f91e43f9182d5d3fa20c546be5c1203dacf076
gandib <55914763+gandib@users.noreply.github.com>
2024-11-01 21:30:35 +0600

    Project initalised

---
+++ src/app/modules/User/user.service.ts
@@ -0,0 +1,32 @@
+import { PrismaClient, UserRole } from "@prisma/client";
+import * as bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+const createAdmin = async (payload: any) => {
+  const hashedPassword = await bcrypt.hash(payload.password, 12);
+
+  const userData = {
+    email: payload.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN,
+  };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdAdminData = await transactionClient.admin.create({
+      data: payload.admin,
+    });
+
+    return createdAdminData;
+  });
+
+  return result;
+};
+
+export const userServices = {
+  createAdmin,
+};
