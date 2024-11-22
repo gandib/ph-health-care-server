7205e665f8a67b81b4da7886bf4e4d5290ddcfde
gandib <55914763+gandib@users.noreply.github.com>
2024-11-22 23:43:16 +0600

    Part 8 er 3 completed

--- src/app/middlewares/globalErrorHandlers.ts
+++ src/app/middlewares/globalErrorHandlers.ts
@@ -7,6 +7,16 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  if (error.code === "P2002") {
+    const target = error.meta.target[0];
+    return res.status(httpStatus.BAD_REQUEST).json({
+      success: false,
+      message: `${
+        target[0].toUpperCase() + target.substring(1)
+      } is already exists!`,
+      error,
+    });
+  }
   res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
     success: false,
     message: error?.message || "Something went wrong!",
--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,37 +1,72 @@
-import { addHours, addMinutes, format } from "date-fns";
+import { addHours, addMinutes, format, interval } from "date-fns";
+import prisma from "../../../utils/prisma";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
+import { Schedule } from "@prisma/client";
 
 const createSchedule = async (payload: {
   startDate: string;
   endDate: string;
   startTime: string;
   endTime: string;
-}) => {
+}): Promise<Schedule[]> => {
   const { startDate, endDate, startTime, endTime } = payload;
+  const schedules = [];
   const currentDate = new Date(startDate);
   const lastDate = new Date(endDate);
+  const intervalTime = 30;
 
-  const currentDateTime = `${startDate}T${startTime}:00.000Z`;
-  const startDateTime2 = new Date(currentDateTime);
-  const lastDateTime = `${endDate}T${endTime}:00.000Z`;
-  const endDateTime2 = new Date(lastDateTime);
-  console.log(startDateTime2, endDateTime2);
+  // const currentDateTime = `${startDate}T${startTime}:00.000Z`;
+  // const startDateTime2 = new Date(currentDateTime);
+  // const lastDateTime = `${endDate}T${endTime}:00.000Z`;
+  // const endDateTime2 = new Date(lastDateTime);
+  // console.log(startDateTime2, endDateTime2);
 
-  //   while (currentDate <= lastDate) {
-  //     const startDateTime = new Date(
-  //       addHours(
-  //         `${format(currentDate, "yyyy-MM-dd")}`,
-  //         Number(startTime.split(":")[0])
-  //       )
-  //     );
+  if (startDate > endDate || startTime > endTime) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Start date and time should not be bigger than end date and time!"
+    );
+  }
 
-  //     const endDateTime = new Date(
-  //       addHours(
-  //         `${format(lastDate, "yyyy-MM-dd")}`,
-  //         Number(endTime.split(":")[0])
-  //       )
-  //     );
-  //     console.log(startDateTime, endDateTime);
-  //   }
+  while (currentDate <= lastDate) {
+    const startDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(startTime.split(":")[0])
+        ),
+        Number(startTime.split(":")[1])
+      )
+    );
+
+    let endDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(endTime.split(":")[0])
+        ),
+        Number(endTime.split(":")[1])
+      )
+    );
+
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, intervalTime),
+      };
+
+      const result = await prisma.schedule.create({
+        data: scheduleData,
+      });
+      schedules.push(result);
+
+      startDateTime.setMinutes(startDateTime.getMinutes() + intervalTime);
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+  return schedules;
 };
 
 export const scheduleServices = {
